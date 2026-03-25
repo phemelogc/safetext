fe35334a2a50aa3bd6207cae93a86f4b1b4a4a10
phemelogc <phemelogc@gmail.com>
2026-03-25 20:58:35 +0200

    fixed api connection between model and app(had to change device ip)

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,5 +1,5 @@
 /// Base URL for the SafeText backend. Change for localhost or another host.
-const String apiBaseUrl = 'http://192.168.197.1:8000';
+const String apiBaseUrl = 'http://5.4.8.246:8000';
 
 String get predictUrl => '$apiBaseUrl/predict';
 String get feedbackUrl => '$apiBaseUrl/feedback';
